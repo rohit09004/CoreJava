e22290dda3b9cffb04982ed4bf00b6c4c81d5d48
ROHIT <ROHIT@DESKTOP-8SI0GBB>
2022-08-19 21:37:14 +0530

    updated code

---
+++ javapractise/src/javapractise/PractiseCoreJava.java
@@ -0,0 +1,11 @@
+package javapractise;
+
+public class PractiseCoreJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" Practise Core Java ");
+		}
+
+}
